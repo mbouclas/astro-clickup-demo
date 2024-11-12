16caf7b61c5f19b6a623d68b64f36bcdaeb7e7e9
mbouclas <mbouclas@gmail.com>
2024-11-12 16:18:41 +0200

    auth wrapper

---
+++ src/components/auth-wrapper.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import Auth from './auth.svelte';
+    import {user} from '../stores/user.store';
+    
+    let isLoggedIn = false
+onMount(async () => {
+    const cached = localStorage.getItem('user');
+    if (cached) {
+        user.set(JSON.parse(cached));
+        isLoggedIn = true
+    }
+})
+
+</script>
+{#if $user === null}
+<Auth />
+{:else}
+<slot></slot>
+
+{/if}
--- src/components/auth.svelte
+++ src/components/auth.svelte
@@ -1,13 +1,16 @@
 <script>
     import { onMount } from "svelte";
-    import {writable} from 'svelte/store';
+    import {user} from '../stores/user.store';
     import { PublicClientApplication } from "@azure/msal-browser";
     import { msalConfig, loginRequest } from "../services/login";
+
+
   
     let msalInstance;
-    let user = writable(null);
+    
 
 onMount(async () => {
+  
   msalInstance = new PublicClientApplication(msalConfig);
   await msalInstance.initialize();
 });
@@ -17,11 +20,14 @@ async function signIn() {
     const loginResponse = await msalInstance.loginPopup(loginRequest);
     console.log("id_token acquired at: " + new Date().toString());
     console.log(loginResponse);
+    user.set(loginResponse);
+    localStorage.setItem('user', JSON.stringify(loginResponse));
   } catch (error) {
     console.error(error);
   }
 }
   </script>
+
   {#if $user === null}
   
   <button on:click={signIn} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
